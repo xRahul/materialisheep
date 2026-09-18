e4295e79faedc6f87e9159da865676d83849092d
Rahul Jain <1639945+xRahul@users.noreply.github.com>
2026-09-18 04:54:08 +0000

    fix(tests): update NetworkModuleTest for OkHttp 5.4.0 Chain API
    OkHttp 5.4.0 adds new abstract members to Interceptor.Chain
    (withDns, withSocketFactory etc. plus dns/socketFactory vals).
    Add stubs returning this/default to satisfy compiler.

--- app/src/test/java/io/github/sheepdestroyer/materialisheep/NetworkModuleTest.kt
+++ app/src/test/java/io/github/sheepdestroyer/materialisheep/NetworkModuleTest.kt
@@ -40,6 +40,23 @@ class NetworkModuleTest {
                     .build()
             }
 
+            override val followRedirects: Boolean get() = false
+            override val followSslRedirects: Boolean get() = false
+            override val dns: okhttp3.Dns get() = okhttp3.Dns.SYSTEM
+            override val socketFactory: javax.net.SocketFactory get() = javax.net.SocketFactory.getDefault()
+            override val retryOnConnectionFailure: Boolean get() = true
+            override val authenticator: okhttp3.Authenticator get() = okhttp3.Authenticator.NONE
+            override val cookieJar: okhttp3.CookieJar get() = okhttp3.CookieJar.NO_COOKIES
+            override val cache: okhttp3.Cache? get() = null
+            override val proxy: java.net.Proxy? get() = null
+            override val proxySelector: java.net.ProxySelector get() = java.net.ProxySelector.getDefault()
+            override val proxyAuthenticator: okhttp3.Authenticator get() = okhttp3.Authenticator.NONE
+            override val sslSocketFactoryOrNull: javax.net.ssl.SSLSocketFactory? get() = null
+            override val x509TrustManagerOrNull: javax.net.ssl.X509TrustManager? get() = null
+            override val hostnameVerifier: javax.net.ssl.HostnameVerifier get() = javax.net.ssl.HttpsURLConnection.getDefaultHostnameVerifier()
+            override val certificatePinner: okhttp3.CertificatePinner get() = okhttp3.CertificatePinner.DEFAULT
+            override val connectionPool: okhttp3.ConnectionPool get() = okhttp3.ConnectionPool()
+            override val eventListener: okhttp3.EventListener get() = okhttp3.EventListener.NONE
             override fun connection(): okhttp3.Connection? = null
             override fun call(): okhttp3.Call = throw UnsupportedOperationException()
             override fun connectTimeoutMillis(): Int = 10000
@@ -48,6 +65,20 @@ class NetworkModuleTest {
             override fun withReadTimeout(timeout: Int, unit: java.util.concurrent.TimeUnit): Interceptor.Chain = this
             override fun writeTimeoutMillis(): Int = 10000
             override fun withWriteTimeout(timeout: Int, unit: java.util.concurrent.TimeUnit): Interceptor.Chain = this
+            override fun withDns(dns: okhttp3.Dns): Interceptor.Chain = this
+            override fun withSocketFactory(socketFactory: javax.net.SocketFactory): Interceptor.Chain = this
+            override fun withRetryOnConnectionFailure(retryOnConnectionFailure: Boolean): Interceptor.Chain = this
+            override fun withAuthenticator(authenticator: okhttp3.Authenticator): Interceptor.Chain = this
+            override fun withCookieJar(cookieJar: okhttp3.CookieJar): Interceptor.Chain = this
+            override fun withCache(cache: okhttp3.Cache?): Interceptor.Chain = this
+            override fun withProxy(proxy: java.net.Proxy?): Interceptor.Chain = this
+            override fun withProxySelector(proxySelector: java.net.ProxySelector): Interceptor.Chain = this
+            override fun withProxyAuthenticator(proxyAuthenticator: okhttp3.Authenticator): Interceptor.Chain = this
+            override fun withSslSocketFactory(sslSocketFactory: javax.net.ssl.SSLSocketFactory?, x509TrustManager: javax.net.ssl.X509TrustManager?): Interceptor.Chain = this
+            override fun withHostnameVerifier(hostnameVerifier: javax.net.ssl.HostnameVerifier): Interceptor.Chain = this
+            override fun withCertificatePinner(certificatePinner: okhttp3.CertificatePinner): Interceptor.Chain = this
+            override fun withConnectionPool(connectionPool: okhttp3.ConnectionPool): Interceptor.Chain = this
+
         }
     }
 
